b2f0ca8c8a0b2e6e20aabfdc660c3c11be776f72
Hoang-Van-Duong Hoang-Van-Duong <hoang.duong@iem.thm.de>
2024-07-06 14:55:24 +0200

    revamp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,3 @@
-let slider = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let dots = document.querySelectorAll('.slider .dots li');
-
-let lengthItems = items.length - 1;
-let active = 0;
-next.onclick = function(){
-    active = active + 1 <= lengthItems ? active + 1 : 0;
-    reloadSlider();
-}
-prev.onclick = function(){
-    active = active - 1 >= 0 ? active - 1 : lengthItems;
-    reloadSlider();
-}
-let refreshInterval = setInterval(()=> {next.click()}, 8000);
-function reloadSlider(){
-    slider.style.left = -items[active].offsetLeft + 'px';
-    // 
-    let last_active_dot = document.querySelector('.slider .dots li.active');
-    last_active_dot.classList.remove('active');
-    dots[active].classList.add('active');
-
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(()=> {next.click()}, 8000);
-
-    
-}
-
-dots.forEach((li, key) => {
-    li.addEventListener('click', ()=>{
-         active = key;
-         reloadSlider();
-    })
-})
-window.onresize = function(event) {
-    reloadSlider();
-};
-
-
-
-
 
 
 
@@ -52,7 +9,7 @@ let next1 = document.getElementById('next1');
 let prev1 = document.getElementById('prev1');
 let dots1 = document.querySelectorAll('.slider1 .dots1 li');
 
-let lengthItems1 = items1.length - 1;
+let lengthItems1 = items1.length - 2;
 let active1 = 0;
 next1.onclick = function(){
     active1 = active1 + 1 <= lengthItems1 ? active1 + 1 : 0;
